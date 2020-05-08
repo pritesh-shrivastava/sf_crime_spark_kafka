628fda8ed5409e75f6294091ac9f4871bbd96180
pritesh-shrivastava <pritesh2312@gmail.com>
2020-05-08 17:48:28 +0530

    [PS] Update files from workspace

--- producer_server.py
+++ producer_server.py
@@ -1,6 +1,7 @@
 from kafka import KafkaProducer
 import json
 import time
+import logging
 
 
 class ProducerServer(KafkaProducer):
@@ -12,14 +13,15 @@ class ProducerServer(KafkaProducer):
 
     #TODO we're generating a dummy data
     def generate_data(self):
-        with open(self.input_file) as f:
-            for line in f:
+        with open(self.input_file, 'r') as f:
+            data = json.load(f)
+            for line in data:
                 message = self.dict_to_binary(line)
                 # TODO send the correct data
-                self.send()
+                self.send(self.topic, message)
                 time.sleep(1)
 
     # TODO fill this in to return the json dictionary to binary
     def dict_to_binary(self, json_dict):
-        return 
+        return(json.dumps(json_dict).encode('utf-8'))
         
